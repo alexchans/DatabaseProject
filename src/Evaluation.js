f7bf39f090ca14bab614b37ac3c4206bfc02c0d8
Alex Chen <alexchen@smu.edu>
2024-04-28 21:24:03 -0500

    Close to get done with evaluations

--- src/Evaluation.js
+++ src/Evaluation.js
@@ -5,9 +5,19 @@ function Evaluation() {
     const [state, setState] = useState({
         degreeId: '',
         semester: '',
-        instructorId: ''
+        instructorId: '',
+        objectiveCode: '',
+        method: '',
+        levelACount: 0, // Assuming initial value should be 0
+        levelBCount: 0,
+        levelCCount: 0,
+        levelFCount: 0,
+        improvementSuggestion: '',
+        sectionNumber: "",
+        courseNumber: ""
     });
     const [sections, setSections] = useState([]); // State to hold sections data
+    const [evaluations, setEvaluations] = useState([]); // State to hold sections data
 
     const handleChange = (event) => {
         const { id, value } = event.target;
@@ -26,50 +36,168 @@ function Evaluation() {
         }
     };
 
+    const handleRequest = (courseNumber, sectionNumber) => async (event) => {
+        console.log("handleRequest called with:", courseNumber, sectionNumber); // Ensure this gets logged
+        try {
+            const response = await axios.get(`http://localhost:8080/evaluations/findSpecifics/${state.degreeId}/${courseNumber}/${sectionNumber}`);
+            setEvaluations(prevEvaluations => [...prevEvaluations, response.data]);
+        } catch (error) {
+        }
+    };
+
+    const handleEvaluationSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const evaluationData = {
+                degreeId: state.degreeId,
+                sectionNumber: state.sectionNumber,
+                courseNumber: state.courseNumber,
+                objectiveCode: state.objectiveCode,
+                method: state.method,
+                levelACount: state.levelACount,
+                levelBCount: state.levelBCount,
+                levelCCount: state.levelCCount,
+                levelFCount: state.levelFCount,
+                improvementSuggestion: state.improvementSuggestion
+            };
+            await axios.post(`http://localhost:8080/evaluations/`, evaluationData);
+        } catch (error) {
+        }
+    };
     return (
         <div>
             <h1>Enter Evaluation</h1>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="degreeId">Degree ID</label>
-                <div>
-                    <input
-                        type="text"
-                        id="degreeId"
-                        value={state.degreeId}
-                        onChange={handleChange}
-                    />
-                </div>
-                <label htmlFor="semester">Semester</label>
-                <div>
-                    <input
-                        type="text"
-                        id="semester"
-                        value={state.semester}
-                        onChange={handleChange}
-                    />
-                </div>
-                <label htmlFor="instructorId">Instructor ID</label>
-                <div>
-                    <input
-                        type="text"
-                        id="instructorId"
-                        value={state.instructorId}
-                        onChange={handleChange}
-                    />
-                </div>
+                <label htmlFor="degreeId">Degree ID: </label>
+                <input
+                    type="text"
+                    id="degreeId"
+                    value={state.degreeId}
+                    onChange={handleChange}
+                />
+                <label htmlFor="semester">Semester: </label>
+
+                <input
+                    type="text"
+                    id="semester"
+                    value={state.semester}
+                    onChange={handleChange}
+                />
+                <label htmlFor="instructorId">Instructor ID: </label>
+
+                <input
+                    type="text"
+                    id="instructorId"
+                    value={state.instructorId}
+                    onChange={handleChange}
+                />
                 <button type="submit">Request Data</button>
             </form>
             <div>
                 {sections.map((section, index) => (
-                    <div key={index} className='flex'>
-                        <p>Section Number: {section.sectionNumber}</p>
-                        <p>Course Number: {section.courseNumber}</p>
-                        <p>Semester: {section.semester}</p>
-                        <p>Year: {section.year}</p>
-                        <p>Enrolled Students: {section.enrolledStudents}</p>
+                    <div key={index}>
+                        <div className='flex'>
+                            <p>Section Number: {section.sectionNumber}</p>
+                            <p>Course Number: {section.courseNumber}</p>
+                            <p>Semester: {section.semester}</p>
+                            <p>Year: {section.year}</p>
+                            <p>Enrolled Students: {section.enrolledStudents}</p>
+                            <button onClick={handleRequest(section.courseNumber, section.sectionNumber)}>Request Evaluations</button>
+                        </div>
                     </div>
                 ))}
             </div>
+            <form onSubmit={handleEvaluationSubmit}>
+                <label htmlFor="objectiveCode">Objective Code: </label>
+                <input
+                    type="text"
+                    id="objectiveCode"
+                    value={state.objectiveCode}
+                    onChange={handleChange}
+                />
+                <label htmlFor="method">Method: </label>
+                <input
+                    type="text"
+                    id="method"
+                    value={state.method}
+                    onChange={handleChange}
+                />
+                <label htmlFor="levelACount">Level A Count: </label>
+                <input
+                    type="number"
+                    id="levelACount"
+                    value={state.levelACount}
+                    onChange={handleChange}
+                />
+                <label htmlFor="levelBCount">Level B Count: </label>
+                <input
+                    type="number"
+                    id="levelBCount"
+                    value={state.levelBCount}
+                    onChange={handleChange}
+                />
+                <label htmlFor="levelCCount">Level C Count: </label>
+                <input
+                    type="number"
+                    id="levelCCount"
+                    value={state.levelCCount}
+                    onChange={handleChange}
+                />
+                <label htmlFor="levelFCount">Level F Count: </label>
+                <input
+                    type="number"
+                    id="levelFCount"
+                    value={state.levelFCount}
+                    onChange={handleChange}
+                />
+                <label htmlFor="improvementSuggestion">Improved Suggestions: </label>
+                <input
+                    type="text"
+                    id="improvementSuggestion"
+                    value={state.improvementSuggestion}
+                    onChange={handleChange}
+                />
+                <label htmlFor="degreeId">Degree ID: </label>
+                <input
+                    type="text"
+                    id="degreeId"
+                    value={state.degreeId}
+                    onChange={handleChange}
+                />
+                <label htmlFor="sectionNumber">Section Number: </label>
+                <input
+                    type="text"
+                    id="sectionNumber"
+                    value={state.sectionNumber}
+                    onChange={handleChange}
+                />
+                <label htmlFor="courseNumber">Course Number: </label>
+                <input
+                    type="text"
+                    id="courseNumber"
+                    value={state.courseNumber}
+                    onChange={handleChange}
+                />
+                <button type="submit">Create Evaluation</button>
+            </form>
+            {evaluations.map((sectionEvaluations, index) => (
+                <div key={index}>
+                    {sectionEvaluations.map((evaluation, evalIndex) => (
+                        <div key={evalIndex}>
+                            <p>Degree ID: {evaluation.degreeId}</p>
+                            <p>Course Number: {evaluation.courseNumber}</p>
+                            <p>Section Number: {evaluation.sectionNumber}</p>
+                            <p>Objective Code: {evaluation.objectiveCode}</p>
+                            <p>Method: {evaluation.method}</p>
+                            <p>Level A Count: {evaluation.levelACount}</p>
+                            <p>Level B Count: {evaluation.levelBCount}</p>
+                            <p>Level C Count: {evaluation.levelCCount}</p>
+                            <p>Level F Count: {evaluation.levelFCount}</p>
+                            <p>Improved Suggestions: {evaluation.improvementSuggestion}</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 }
