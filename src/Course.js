26d007b5f3bb67c5e08827f477820e3f5f124153
ap-1678 <poses.partage_0h@icloud.com>
2025-07-08 13:19:58 -0500

    testing git push

--- src/Course.js
+++ src/Course.js
@@ -1,6 +1,6 @@
 // import React, { useState } from 'react';
 // import axios from 'axios';
-//
+// perez test
 // function Course() {
 //     const [state, setState] = useState({
 //         courseNumber: '',
